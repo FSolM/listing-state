86ed84ba8ee4ca59973ce96ab68ffd8657e2d5b8
FSolM <fersolmarin@gmail.com>
2020-01-29 01:45:50 -0600

    Base Session Logic Added

---
+++ src/helpers/session.js
@@ -0,0 +1,11 @@
+// ToDo
+// Checks if there's a current user active
+// If active, you cannot enter to LogIn and SignUp pages, redirects to main
+// If not, you cannot access to the app, redirects to LogIn
+
+// Maybe something to do with cookies like a session saver?
+// Need to add an "Keep session open" in LogIn/SignUp
+
+export default {
+  currentUser: null
+};
