14339b134b94d41308a50c6d25f56fec5981b803
FSolM <fersolmarin@gmail.com>
2020-02-02 18:15:33 -0600

    Config route Added

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import SignUp from './components/registry/SignUp';
 import LogIn from './components/registry/LogIn';
+import UserView from './components/UserView';
 import Listing from './components/Listing';
 import ListingForm from './components/ListingForm';
 
 function App() {
   return (
-    <div className="App">
+    <div className = 'App'>
       <Switch>
         <Route path = '/SignUp'>
           <SignUp />
         </Route>
         <Route path = '/LogIn'>
           <LogIn />
+        </Route>
+        <Route path = '/Config'>
+          <UserView />
         </Route>
         <Route path = '/New-Listing'>
           <ListingForm />
