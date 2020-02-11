c1774b3305b1027edc32b08e1c9f33adefaad289
FSolM <fersolmarin@gmail.com>
2020-02-10 23:01:15 -0600

    redux Init

--- src/index.js
+++ src/index.js
@@ -2,15 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+
 import App from './App';
 
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(rootReducer)
+
 import './css/tejuino.css'
 
 ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>, document.getElementById('root'));
+    <Provider store = { store }>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>, document.getElementById('root'));
 
 serviceWorker.register();
