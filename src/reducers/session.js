2f0bcb0d31cc9258347052e41192da856cc4572d
FSolM <fersolmarin@gmail.com>
2020-02-14 14:54:21 -0600

    Cookies Implemented

--- src/reducers/session.js
+++ src/reducers/session.js
@@ -1,8 +1,9 @@
-const initialState = { user: '' };
+const initialState = { user: document.cookies.split('=')[1] || '' };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'SET_SESSION':
+      document.cookie = `user=${action.payload}`;
       return Object.assign({}, state, {user: action.payload });
     default:
       return state;
